20cb5fb5c8bfb2ced7740c6587551285106a718d
Chris Barnes <barnesc@janelia.hhmi.org>
2019-09-20 13:56:37 -0400

    Bump version: 0.2.0 → 0.2.1

--- f_it/__init__.py
+++ f_it/__init__.py
@@ -6,7 +6,7 @@ from .fit import FIt
 
 __author__ = """Chris L. Barnes"""
 __email__ = "chrislloydbarnes@gmail.com"
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 __version_info__ = tuple(int(n) for n in __version__.split("."))
 
 __all__ = ["FIt"]
