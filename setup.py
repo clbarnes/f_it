d95a333d0b647e3432f8f0359801116fb8b4553f
Chris Barnes <barnesc@janelia.hhmi.org>
2019-10-16 16:56:03 -0400

    Test against python 3.8

--- setup.py
+++ setup.py
@@ -35,6 +35,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     description="Iterator class for functional programming",
     install_requires=requirements,
